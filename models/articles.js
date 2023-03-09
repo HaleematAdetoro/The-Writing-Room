40f84471f254f5649c98b0d7a80e7701703156c5
Haleemat Adetoro <haleematadetoro24@gmail.com>
2023-03-09 02:35:52 -0600

    refactoring

--- models/articles.js
+++ models/articles.js
@@ -34,33 +34,5 @@ module.exports = (sequelize, DataTypes) => {
     });
         return Article;
 };  
-    // const Article = sequelize.define("Article", {
-    //     id: {
-    //         type: DataTypes.INTEGER,
-    //         autoIncrement: true,
-    //         primaryKey: true
-    //     },
-    //     title: {
-    //         type: DataTypes.STRING,
-    //         allowNull: false
-    //     },
-    //     body: {
-    //         type: DataTypes.STRING,
-    //         allowNull: false
-    //     }
-    // }, {
-    //     tableName: 'articles',
-    // })
 
-    // class Article extends Model {
-    //     static associate({comments}) {
-    //         this.hasMany(comment, {foreignKey: 'articleId', as: 'comments'})
-    //     }
-    // }
-    // Article.associate = function (models) {
-    //     Article.hasMany(models.comments, {
-    //         onDelete: "CASCADE",
-    //     });
-    //     Article.belongsTo(models.users)
-    // };
 
--- models/comments.js
+++ models/comments.js
@@ -43,24 +43,3 @@ module.exports = (sequelize, DataTypes) => {
 
 
 
-// module.exports = (sequelize, DataTypes) => {
-//     const Comment = sequelize.define("Comment", {
-//         id: {
-//             type: DataTypes.INTEGER,
-//             autoIncrement: true,
-//             primaryKey: true
-//         },
-//         comment: {
-//             type: DataTypes.STRING,
-//             allowNull: false
-//         }
-//     }, {
-//         tableName: 'comments'
-//     })
-
-//     Comment.associate = function (models) {
-//         Comment.belongsTo(models.user);
-//         Comment.belongsTo(models.articles);
-//     };
-//     return Comment;
-// }
